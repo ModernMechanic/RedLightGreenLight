72999235de7be696c9b8118e43f16479dede4949
mm-dustindavignon <dustin@modernmechanic.com>
2016-07-22 10:05:42 -0700

    Initial Commit

---
+++ RedLightGreenLight/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RedLightGreenLight
+//
+//  Created by dustin on 7/22/16.
+//  Copyright © 2016 Modern Mechanic. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
